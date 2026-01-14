6426eb5c44dfbe809b4b7dc53791d4892b672593
USER <noamjacobi2009@gmail.com>
2026-01-14 17:08:32 +0200

    עותק אחרון עם ירושה של SPRITES

--- characters.py
+++ characters.py
@@ -1,40 +1,68 @@
 import arcade
 import random
 
-PACMAN_COLOR = arcade.color.YELLOW
-GHOST_COLOR = arcade.color.RED
-COIN_COLOR = arcade.color.YELLOW
-WALL_COLOR = arcade.color.BLUE
+coin_texture = arcade.make_circle_texture(20, arcade.color.YELLOW)
+player_texture = arcade.make_circle_texture(50, arcade.color.YELLOW)
+enemy_texture = arcade.make_circle_texture(50, arcade.color.RED)
+wall_texture = arcade.make_rectangle_texture(50, 50, arcade.color.BLUE)
 
-pacman_texture = arcade.make_circle_texture(50, PACMAN_COLOR)
-ghost_texture = arcade.make_circle_texture(50, GHOST_COLOR)
-coin_texture = arcade.make_circle_texture(20, COIN_COLOR)
-wall_texture = arcade.make_rectangle_texture(50, 50, WALL_COLOR)
-
-class Character(arcade.Sprite):
-    def __init__(self, texture, speed=5):
-        super().__init__()
-        self.texture = texture
-        self.speed = speed
-
-class Pacman(Character):
-    def __init__(self):
-        super().__init__(pacman_texture)
-
-class Ghost(Character):
-    def __init__(self):
-        super().__init__(ghost_texture)
-
-    def move_random(self):
-        self.center_x += random.choice([-self.speed, 0, self.speed])
-        self.center_y += random.choice([-self.speed, 0, self.speed])
 
 class Coin(arcade.Sprite):
-    def __init__(self):
+    def __init__(self, center_x, center_y, value=10):
         super().__init__()
         self.texture = coin_texture
+        self.center_x = center_x
+        self.center_y = center_y
+        self.value = value
+
+
+class Character(arcade.Sprite):
+    def __init__(self, center_x, center_y, texture, speed=1):
+        super().__init__()
+        self.texture = texture
+        self.center_x = center_x
+        self.center_y = center_y
+        self.speed = speed
+        self.change_x = 0
+        self.change_y = 0
+
+
+class Player(Character):
+    def __init__(self, center_x, center_y):
+        super().__init__(center_x, center_y, player_texture, speed=1)
+        self.score = 0
+        self.lives = 3
+
+    def move(self):
+        self.center_x += self.change_x * self.speed
+        self.center_y += self.change_y * self.speed
+
+
+class Enemy(Character):
+    def __init__(self, center_x, center_y):
+        super().__init__(center_x, center_y, enemy_texture, speed=1)
+        self.time_to_change_direction = 0
+
+    def pick_new_direction(self):
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (0, 0)]
+        direction = random.choice(directions)
+        self.change_x = direction[0]
+        self.change_y = direction[1]
+        self.time_to_change_direction = random.uniform(0.3, 1.0)
+
+    def update(self, delta_time=1/60):
+        self.time_to_change_direction -= delta_time
+
+        if self.time_to_change_direction <= 0:
+            self.pick_new_direction()
+
+        self.center_x += self.change_x * self.speed
+        self.center_y += self.change_y * self.speed
+
 
 class Wall(arcade.Sprite):
-    def __init__(self):
+    def __init__(self, center_x, center_y):
         super().__init__()
         self.texture = wall_texture
+        self.center_x = center_x
+        self.center_y = center_y
